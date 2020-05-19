8e47e508e54379c8dd172601ee76b56628db91ad
hmfcookie <mfcookie99@gmail.com>
2020-05-19 22:13:30 +0800

    95. 不同的二叉搜索树 II

---
+++ src/com/mfcookie/tree/LeetCode95.java
@@ -0,0 +1,72 @@
+package com.mfcookie.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author hmfcookie
+ * @date 2020/5/19
+ */
+public class LeetCode95 {
+    public static void main(String[] args) {
+        Solution95 solution95 = new Solution95();
+        List<TreeNode> treeNodes = solution95.generateTrees(3);
+        System.out.println("treeNodes = " + treeNodes);
+    }
+}
+
+/**
+ * 95. 不同的二叉搜索树 II
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出:
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释:
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ */
+class Solution95 {
+    public List<TreeNode> generateTrees(int n) {
+        if (n <= 0) {
+            return new LinkedList<>();
+        }
+        LinkedList<TreeNode> list = helper(1, n);
+        return list;
+    }
+
+    private LinkedList<TreeNode> helper(int start, int end) {
+        LinkedList<TreeNode> linkedList = new LinkedList<>();
+        if (start > end) {
+            linkedList.add(null);
+            return linkedList;
+        }
+        for (int i = start; i <= end; i++) {
+            LinkedList<TreeNode> left = helper(start, i - 1);
+            LinkedList<TreeNode> right = helper(i + 1, end);
+            for (TreeNode sonLeft : left) {
+                for (TreeNode sonRight : right) {
+                    TreeNode node = new TreeNode(i);
+                    node.left = sonLeft;
+                    node.right = sonRight;
+                    linkedList.add(node);
+                }
+            }
+        }
+        return linkedList;
+    }
+}
+
