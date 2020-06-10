5b2427d45e43d738f0bb2e984048f3c035de6154
hmfcookie <mfcookie99@gmail.com>
2020-06-10 15:47:17 +0800

    100. 相同的树

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LeetCodeStudy
 
 记录`leetcode`的刷题
+
+[100. 相同的树](./src/com/mfcookie/tree/LeetCode100.java)
---
+++ src/com/mfcookie/tree/LeetCode100.java
@@ -0,0 +1,57 @@
+package com.mfcookie.tree;
+
+/**
+ * @author hmfcookie
+ * @date 2020/6/10
+ */
+public class LeetCode100 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * 100. 相同的树
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:       1         1
+ * / \       / \
+ * 2   3     2   3
+ * <p>
+ * [1,2,3],   [1,2,3]
+ * <p>
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:      1          1
+ * /           \
+ * 2             2
+ * <p>
+ * [1,2],     [1,null,2]
+ * <p>
+ * 输出: false
+ */
+
+/**
+ * 时间复杂度 : O(N)O(N)，其中 N 是树的结点数，因为每个结点都访问一次。
+ * <p>
+ * 空间复杂度 : 最优情况（完全平衡二叉树）时为 O(\log(N))O(log(N))，最坏情况下（完全不平衡二叉树）时为 {O}(N)O(N)，用于维护递归栈。
+ */
+class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
